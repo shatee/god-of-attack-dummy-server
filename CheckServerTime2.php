ec44240aeaa6424a7c42c7262c0f9667b643f2bd
shate <shate@al02.com>
2017-03-16 00:14:44 +0900

    initialize

---
+++ CheckServerTime2.php
@@ -0,0 +1,2 @@
+<?php
+echo strtotime(file_get_contents('./dateOffset.txt'));
---
+++ checkuserdate.php
@@ -0,0 +1,1 @@
+<root><item><?php echo date('Y-m-d', strtotime(file_get_contents('./dateOffset.txt'))); ?></item></root>
